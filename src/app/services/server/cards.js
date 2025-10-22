bc9103dc8209682f962b5af1d2c3665843a4da9a
Tami-Mendelman <tami5700804@gmail.com>
2025-10-22 15:37:20 +0300

    add change

--- src/app/services/server/cards.js
+++ src/app/services/server/cards.js
@@ -1,8 +1,6 @@
-// src/app/services/server/cards.js
 import { getDb } from "./mongo";
 
 export async function fetchCards() {
   const db = await getDb();
-  // לפי המבנה אצלך: collection נקרא 'shop'
   return db.collection("shop").find({}).toArray();
 }
